30080459c60f4db710e21b3d84e61b061834a6a3
Alex Girard <alexgirard@mac.home>
2025-03-05 14:12:58 -0500

    Feature: v0 of image sampling

--- src/renderer/scenes/ImportImageScene.cpp
+++ src/renderer/scenes/ImportImageScene.cpp
@@ -1,6 +1,5 @@
 ﻿#include "ImportImageScene.h"
 
-#include "Macros.h"
 #include "imgui.h"
 #include "ofLog.h"
 
@@ -9,11 +8,20 @@ void ImportImageScene::draw()
 {
     ImGui::Begin(this->GetName().c_str());
     if (ImGui::Button("Import image")) { this->ImportImageButtonPressed(); }
+    if (ImGui::Button("Sample image")) { this->SampleImage(); }
+    if (ImGui::Button("Generate new images")) { this->GenerateNewImages(); }
     ImGui::End();
 
     for (auto& [image, position]: images)
     {
         image.draw(position);
+    }
+
+    float xOffset = 20;
+    for (auto& img : generatedImages)
+    {
+        img.draw(xOffset, 20);
+        xOffset += img.getWidth() + 10;
     }
 }
 
@@ -36,3 +44,45 @@ void ImportImageScene::ImportImage(const std::string& path)
                              (static_cast<float>(ofGetHeight()) / 2.0f) - image.image.getHeight() / 2.0f);
     this->images.push_back(image);
 }
+
+void ImportImageScene::SampleImage()
+{
+    if (images.empty()) return;
+
+    generatedImages.clear();
+    int numParts = 3;
+    for (auto& img : images)
+    {
+        if (img.image.getWidth() < numParts) return;
+
+        int partWidth = img.image.getWidth() / numParts;
+        int height = img.image.getHeight();
+
+        for (int i = 0; i < numParts; i++)
+        {
+            ofImage part;
+            part.allocate(partWidth, height, img.image.getPixels().getImageType());
+            part.cropFrom(img.image, i * partWidth, 0, partWidth, height);
+            generatedImages.push_back(part);
+        }
+    }
+
+    images.clear();
+}
+
+void ImportImageScene::GenerateNewImages()
+{
+    if (generatedImages.empty()) return;
+
+    float xOffset = 20;
+    for (auto& img : generatedImages)
+    {
+        if (img.isAllocated())
+        {
+            img.draw(xOffset, 20);
+            xOffset += img.getWidth() + 10;
+        }
+    }
+}
+
+
